fee20ce0aff7d50387652c5812df8acdaedb8acf
aadeshdhakal <33834901+aadeshdhakal@users.noreply.github.com>
2017-11-27 10:41:05 +0545

    Add files via upload

---
+++ area.py
@@ -0,0 +1,7 @@
+# done by Aadesh Dhakal Roll: 601
+
+PI = 2.1415
+radius = input("Enter the radius: ")
+
+area = PI * float(radius) * float(radius)
+print ("{0}*{1}*{1} = {2:.3f}\n is the required area.".format(PI,radius,area))
---
+++ birthday_song.py
@@ -0,0 +1,7 @@
+# done by Aadesh Dhakal Roll: 601
+
+def birthday(f_name):
+    print ("{0} {1} {2} {3}\n{0} {1} {2} {3}\n{0} {1} {4} {5}\n{0} {1} {2} {3}!!".format("happy","birthday","to","you","dear",f_name))
+
+name = input("Enter your friend's name: ")
+birthday(name)
